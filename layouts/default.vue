03abdcdd6475927fefe48585d788060447833d7a
burak-ekici <78544799+burak-ekici@users.noreply.github.com>
2021-04-14 12:34:41 +0200

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-app>
+      <div class="w-full overflow-hidden">
+        <div class="mb-32 toolbar">
+          <Navbar2 />
+        </div>
+        <Nuxt />
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Navbar2 from "../components/Navbar2";
+import Footer from "../components/Footer";
+export default {
+  components: {
+    Navbar2,
+    Footer,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+html,
+body,
+* {
+  color: #444b54;
+}
+.toolbar {
+  transition: All 0.7s;
+}
+</style>
